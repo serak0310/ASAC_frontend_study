3f46d609baaa64c6f525fe55466e4508723a9b49
Minjeong Kim <serak0310@naver.com>
2023-04-10 13:25:32 +0900

    [add] 더보기 버튼

---
+++ Ch02/wantedCopy.js
@@ -0,0 +1,18 @@
+// 메인 패널 반복
+const container = document.getElementById('careerContentList');
+const wrapper = document.getElementById('carrerContentList_content');
+
+for(let i=0; i<7; i++){
+    const newItem = wrapper.cloneNode(true);
+    container.appendChild(newItem);
+}
+
+// 더보기 버튼
+const moreContent_btn = document.getElementById('moreContent_btn');
+moreContent_btn.addEventListener("click", function(){
+    alert('버튼 클릭됨');
+    for(let i=0; i<8; i++){
+        const newItem = wrapper.cloneNode(true);
+        container.appendChild(newItem);
+    }
+})
