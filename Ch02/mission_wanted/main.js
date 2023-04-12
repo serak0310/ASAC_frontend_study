0ab1752f5b744e94ae17f39d167a7d401db06f3f
Minjeong Kim <serak0310@naver.com>
2023-04-12 10:28:55 +0900

    add : 3초의 로딩후 스피너 숨기기&전체 화면 보이기

--- Ch02/mission_wanted/main.js
+++ Ch02/mission_wanted/main.js
@@ -16,3 +16,9 @@ moreContent_btn.addEventListener("click", function(){
         container.appendChild(newItem);
     }
 })
+
+// 로딩 3초 후, 메인 페이지 show
+setTimeout(function() {
+    document.getElementById('spinner').style.display = 'none'; // 스피너 숨기기
+    document.getElementById('loaded').classList.add('show'); // 로딩 완료 후 보이기
+  }, 3000); // 3초 후 실행
