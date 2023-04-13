422eee7456979d66be4baa93c32e3e2bf50a65b5
Minjeong Kim <serak0310@naver.com>
2023-04-13 16:30:51 +0900

    add : 이메일 정규식 검사 - 경고 텍스트 출력

--- Ch02/mission_wanted/main.js
+++ Ch02/mission_wanted/main.js
@@ -1,4 +1,4 @@
-// 메인 패널 반복
+/*--- 메인 패널 반복 ---*/
 const container = document.getElementById('careerContentList');
 const wrapper = document.getElementById('carrerContentList_content');
 
@@ -7,7 +7,7 @@ for(let i=0; i<7; i++){
     container.appendChild(newItem);
 }
 
-// 더보기 버튼
+/*--- 더보기 버튼 ---*/
 const moreContent_btn = document.getElementById('moreContent_btn');
 moreContent_btn.addEventListener("click", function(){
     // alert('버튼 클릭됨');
@@ -17,13 +17,13 @@ moreContent_btn.addEventListener("click", function(){
     }
 })
 
-// 로딩 3초 후, 메인 페이지 show
+/*--- 로딩 3초 후, 메인 페이지 show ---*/
 setTimeout(function() {
     document.getElementById('spinner').style.display = 'none'; // 스피너 숨기기
     document.getElementById('loaded').classList.add('show'); // 로딩 완료 후 보이기
 }, 3000); // 3초 후 실행
 
-// 로그인 모달 창
+/*---  로그인 모달 창 ---*/
 var login_modal = document.getElementById("login_modal");
 var login_btn = document.getElementById("login-btn");
 var span = document.getElementsByClassName("close")[0];
@@ -44,3 +44,22 @@ window.onclick = function(event) {  // 모달 창 외부 클릭시 모달
 // login_modal.onclick = function() {  // 모달 창 외부 클릭시 모달 
 //     login_modal.style.display = "none";
 // }
+
+/*--- 이메일 형식 검사 ---*/
+let emailValue = ""
+const emailNotValid_txt = document.getElementById('emailNotValid_txt')
+
+function emailCheck(){
+    let emailRegex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+    emailValue = document.getElementById('userEmail').value;
+
+    if (emailValue.match(emailRegex) != null) {
+        // 이메일 형식이 맞다면, 버튼 활성화 + border blue
+        emailNotValid_txt.style.display = "none";
+    }
+    else {
+        // 이메일 형식이 아니라면, 버튼 비활성화 + border red
+        emailNotValid_txt.style.display = "block";
+    }
+}
+
