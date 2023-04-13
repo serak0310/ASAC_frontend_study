c6963517bb0d7db3395e1ad4d09e01beaaefeb49
Minjeong Kim <serak0310@naver.com>
2023-04-13 17:12:34 +0900

    add : 이메일 형식 검사 결과에 따른 회원가입 버튼 활성화/비활성화

--- Ch02/mission_wanted/main.js
+++ Ch02/mission_wanted/main.js
@@ -37,8 +37,9 @@ login_btn.onclick = function () {
 
 window.onclick = function (event) {
   // 모달 창 외부 클릭시 모달
-  if (event.target == login_modal) {
+  if (event.target == login_modal || event.target == join_modal) {
     login_modal.style.display = "none";
+    join_modal.style.display = "none";
     body.style.overflow = "auto"; // 모달 제외 외부 스크롤 가능
   }
 };
@@ -62,6 +63,7 @@ function emailCheck() {
     emailNotValid_txt.style.display = "none";
     continue_btn.style.backgroundColor = "#f2f4f7";
     continue_btn.style.color = "#ccc";
+    continue_btn.disabled = true;
     userEmail_tf.style.borderColor="rgb(225, 226, 227)";
   } else {
     // 입력이 있다면
@@ -70,13 +72,25 @@ function emailCheck() {
       emailNotValid_txt.style.display = "none";
       continue_btn.style.backgroundColor = "#36f";
       continue_btn.style.color = "#fff";
+      continue_btn.disabled = false;
       userEmail_tf.style.borderColor="rgb(225, 226, 227)";
     } else {
       // 이메일 형식이 아니라면, 버튼 비활성화 + 경고창O
       emailNotValid_txt.style.display = "block";
       continue_btn.style.backgroundColor = "#f2f4f7";
       continue_btn.style.color = "#ccc";
+      continue_btn.disabled = true;
       userEmail_tf.style.borderColor="#fe415c";
     }
   }
 }
+
+/*---  회원가입 모달 창 ---*/
+var join_modal = document.getElementById("join_modal");
+
+continue_btn.onclick = function () {
+  // 이메일로 계속하기 버튼 클릭시 모달 팝업
+  join_modal.style.display = "block";
+  login_modal.style.display = "none";
+  body.style.overflow = "hidden"; // 모달 제외 외부 스크롤 막기
+};
