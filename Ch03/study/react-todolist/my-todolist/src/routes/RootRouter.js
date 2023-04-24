5ecbc85812ea6f2994f46b82530530155e3c385d
Minjeong Kim <serak0310@naver.com>
2023-04-24 10:12:38 +0900

    [4W] Router

---
+++ Ch03/study/react-todolist/my-todolist/src/routes/RootRouter.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainPage from "../pages/MainPage";
+import SubPage from "../pages/SubPage";
+import TodoList from "../pages/TodoList";
+
+const RootRouter = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+            
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default RootRouter;
