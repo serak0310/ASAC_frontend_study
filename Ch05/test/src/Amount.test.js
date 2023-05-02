3d002f1a476bd845f796fd798f62dc4816d34381
Minjeong Kim <serak0310@naver.com>
2023-05-02 09:31:54 +0900

    [Ch05] test case 추가

--- Ch05/test/src/Amount.test.js
+++ Ch05/test/src/Amount.test.js
@@ -1,14 +1,18 @@
-export default function Amount({ amount }) {
-    const totalAmount = Number(amount) + 3000;
-  
-    return (
-      <>
-        <div>{totalAmount}</div>
-        <div>{amount + "원"}</div>
-      </>
-    );
-  }
-  
-  export function calculateAmount(amount) {
-    return amount * 100;
-  }
+import { render, screen } from '@testing-library/react';
+import Amount, { calculateAmount } from './Amount';
+
+test('3000을 더한 금액이 잘 나오는지 확인', () => {
+  render(<Amount amount={20000}/>);
+  const amountText = screen.getByText(/23000/i);
+  expect(amountText).toBeInTheDocument();
+});
+
+test('원을 포함한 금액이 잘 나오는지 확인', () => {
+  render(<Amount amount={20000}/>);
+  const amountText = screen.getByText(/20000원/i);
+  expect(amountText).toBeInTheDocument();
+});
+
+test("계산함수가 잘 작동하는지 확인", ()=>{
+  expect(calculateAmount(100)).toBe(10000)
+})
